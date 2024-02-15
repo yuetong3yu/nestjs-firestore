8f0d9d20111dad02a09e0c10e85fa74f0b230a6e
chris.yu <yuetong3yu@gmail.com>
2024-02-15 15:18:30 +1100

    Add - validation for create-user-dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly age: number;
 }
